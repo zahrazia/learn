4d192ebc4cdea03bce89feef1724628788b8bcd9
zahrazia <zahra.ziaei.74@gmail.com>
2018-08-22 21:50:41 +0200

    Initial commit

---
+++ test1.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import numpy as np
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    x = np.random.random([10])
+    plt.plot(x)
+    plt.show()
